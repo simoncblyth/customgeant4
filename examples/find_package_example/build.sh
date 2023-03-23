dfd2aad5623cccf9833b90923ef0d5defdeb0de9
Simon C Blyth <simoncblyth@gmail.com>
2023-03-23 16:31:58 +0000

    VERBOSE control when use find_package

--- examples/find_package_example/build.sh
+++ examples/find_package_example/build.sh
@@ -1,4 +1,16 @@
 #!/bin/bash -l 
+usage(){ cat << EOU
+build.sh
+==========
+
+Need CMAKE_PREFIX_PATH setup to find Custom4 for example::
+
+
+EOU
+}
+
+
+arg=$1
 
 sdir=$(pwd)
 name=$(basename $sdir) 
@@ -11,7 +23,11 @@ rm -rf $bdir
 mkdir -p $bdir
 cd $bdir
 
-export CMAKE_PREFIX_PATH=$CMAKE_PREFIX_PATH:/tmp/$USER/customgeant4/install
+if [ "${arg/kludge}" != "$arg" ]; then
+    export CMAKE_PREFIX_PATH=$CMAKE_PREFIX_PATH:/tmp/$USER/customgeant4/install
+fi 
+
+echo $CMAKE_PREFIX_PATH | tr ":" "\n"
 
 cmake $sdir -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=$idir
 
